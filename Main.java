c4bd546137178ef2795cdbf37a00454a682a73c7
Anmol Gaur <134783499+anmol1234576@users.noreply.github.com>
2024-01-06 21:22:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your c
+        //Java program to create a showMessageDialog in JOptionPane
+import javax.swing.JOptionPane;
+
+        public class Main {
+            public static void main(String[] args) {
+                // Create a JOptionPane to display a message dialog
+                // The first parameter (null) specifies the dialog's parent component (usually the main frame)
+                // The second parameter is the message to display ("GFG" in this case)
+                // The third parameter is the title of the dialog ("Geeks Premier League 2023")
+                // The fourth parameter (JOptionPane.INFORMATION_MESSAGE) specifies the message type (information icon)
+                JOptionPane.showMessageDialog(null, "GFG", "Geeks Premier League 2023",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+
+        // Press Alt+Enter with your caret at the highlighted text to see how
+
